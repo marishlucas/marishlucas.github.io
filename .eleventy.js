82432d4f8ca8a6113a4199c47e695ed0f3495b82
lucas <lucas.m.maris@gmail.com>
2024-03-20 14:30:33 +0200

    base config files

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,29 @@
 const markdownIt = require("markdown-it");
 
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-    const md = new markdownIt({
+  const md = new markdownIt({
     html: true
   });
 
   eleventyConfig.addPairedShortcode("markdown", (content) => {
     return md.render(content);
-  }); 
+  });
 
-    eleventyConfig.addPlugin(eleventySass);
-    eleventyConfig.addPassthroughCopy("CNAME");
-    eleventyConfig.addPassthroughCopy("src/assets/img");
-    eleventyConfig.addPassthroughCopy("src/assets/js");
-    eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  // eleventyConfig.addPassthroughCopy("src/assets/img");
+  // eleventyConfig.addPassthroughCopy("src/assets/js");
+  // eleventyConfig.addPassthroughCopy("src/assets/fonts");
 
-    return {
-        dir: {
-            input: "src",
-            output: "dist",
-        }
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
     }
+  }
 };
