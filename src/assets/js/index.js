5f8e5086feba4335f11f474f57b237e47d7b46b4
lucas <lucas.m.maris@gmail.com>
2024-05-22 13:35:09 +0300

    [ style ] - file format

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,6 @@
 document.querySelectorAll(".nav-link").forEach((link) => {
-  console.log(link);
-    if (link.href === window.location.href) {
-        link.classList.add("active");
-        link.setAttribute("aria-current", "page");
-    }
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+    link.setAttribute("aria-current", "page");
+  }
 });
-
