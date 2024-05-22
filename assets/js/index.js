7510d02d88574527a800a5c93c6ddfcd046b88af
marishlucas <marishlucas@users.noreply.github.com>
2024-05-22 11:17:01 +0000

    deploy: b50285afca9f5cc690dc3544f99807fda0c2bb1d

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,11 @@
 document.querySelectorAll(".nav-link").forEach((link) => {
-  console.log(link);
-    if (link.href === window.location.href) {
-        link.classList.add("active");
-        link.setAttribute("aria-current", "page");
-    }
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+    link.setAttribute("aria-current", "page");
+  }
 });
 
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+}
